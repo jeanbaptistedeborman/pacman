ab2139e96fd9389b60d712a8c30f40864e0a6af6
Jean-Baptiste de Borman <jb@noloading.com>
2017-04-24 21:56:15 +0200

    cleanup

--- js/game/livemanager.js
+++ js/game/livemanager.js
@@ -5,14 +5,11 @@ var
     LIFES_NUM = 5,
     lives_num = LIFES_NUM,
     updateView = function (lives_num) {
-
         var display_text = document.getElementById('lives'),
             livesEl_array = document.getElementsByClassName('liveIcon'),
             n;
-        console.log(livesEl_array);
         for (n = 0; n < livesEl_array.length; n++) {
             var el = livesEl_array[n];
-            console.log('n : ', livesEl_array[n]);
             if (n < lives_num) {
                 el.setAttribute('display', 'inline');
             } else {
