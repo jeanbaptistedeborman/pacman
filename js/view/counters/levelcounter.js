75a6a23d10319e2d7886eed433497429da4efe15
Jean-Baptiste de Borman <jb@noloading.com>
2017-04-30 13:34:06 +0200

    refactoring

--- js/view/counters/levelcounter.js
+++ js/view/counters/levelcounter.js
@@ -2,14 +2,14 @@
  * Created by Jean-Baptiste on 11/04/2017.
  */
 var
-    updateView = function (level_num) {
+    display= function (level_num) {
         var
             text_text = document.getElementById('level');
         text_text.textContent = level_num;
     };
 module.exports = {
     set: function (num) {
-        updateView(num);
+        display(num);
     }
 };
 
--- js/view/counters/livemanager.js
+++ js/view/counters/livemanager.js
@@ -4,7 +4,7 @@
 var
     LIVES_NUM = 5,
     lives_num = LIVES_NUM,
-    updateView = function (lives_num) {
+    display = function (lives_num) {
         var
             livesEl_array = document.getElementsByClassName('liveIcon'),
             n;
@@ -17,23 +17,23 @@ var
             }
         }
     };
-updateView(LIVES_NUM);
+display(LIVES_NUM);
 module.exports = {
     decrement: function () {
-        updateView(--lives_num);
+        display(--lives_num);
         if (lives_num === 0) {
             onLivesLost_fun();
         }
     },
     increment: function () {
-        updateView(++lives_num);
+        display(++lives_num);
     },
     set onLivesLost(fun) {
         onLivesLost_fun = fun;
     },
     reset: function () {
         lives_num = LIVES_NUM;
-        updateView(lives_num);
+        display(lives_num);
     }
 };
 
