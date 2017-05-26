71a0868cd1b69e7d358fb24ff7e6288f7c76df44
Jean-Baptiste de Borman <jb@noloading.com>
2017-05-26 18:56:54 +0200

    center texts in bricks

--- js/view/gameobjects/objects/obstacle.js
+++ js/view/gameobjects/objects/obstacle.js
@@ -78,11 +78,12 @@ module.exports = {
                             {
                                 width: gridSize_num,
                                 height: gridSize_num,
+                                'text-anchor':'middle',
                                 fill: 'white',
                                 "font-family": "Arial narrow",
-                                "font-size": "10.5",
-                                x: 1 + config.position.x + n * gridSize_num * Number(config.direction === 'width'),
-                                y: textHeight_num + config.position.y + n * gridSize_num * Number(config.direction === 'height')
+                                "font-size": "9",
+                                x: gridSize_num/2 + config.position.x + n * gridSize_num * Number(config.direction === 'width'),
+                                y: textHeight_num-0.7 + config.position.y + n * gridSize_num * Number(config.direction === 'height')
                             }
                         ),
                         text_node = document.createTextNode(string_array.shift().toUpperCase());
