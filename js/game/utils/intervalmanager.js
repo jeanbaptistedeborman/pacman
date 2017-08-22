56e042f6a6fab32c556d6213bc1831f8e798c754
Jean-Baptiste de Borman <jb@noloading.com>
2017-08-22 19:14:29 +0200

    Jsdoc

--- js/game/utils/intervalmanager.js
+++ js/game/utils/intervalmanager.js
@@ -1,12 +1,27 @@
+/**
+ * @module
+ * @description This wrapper keeps a list of all intervals used in tha application in order to clean them.
+ *
+ *
+ */
+
 "use strict";
 var interval_array = [];
 module.exports = {
+    /**
+     * Cleans all intervals registred through this module.
+     */
     clearAll: function () {
         interval_array.forEach(function (interval_api) {
             interval_api.clear();
         });
         interval_array = [];
     },
+    /**
+     * Wrapper: sets and registers an interval.
+     * @param {function} fun - The interval's callback function.
+     * @param {number} delay_num - The interval's delay.
+     */
     set: function (fun, delay_num) {
         (function () {
             var
