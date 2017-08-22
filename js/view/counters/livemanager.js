ec95de202d0e920b9531fcbe0c6b282d34f88be5
Jean-Baptiste de Borman <jb@noloading.com>
2017-08-22 18:06:57 +0200

    Jsdoc

--- js/view/counters/livemanager.js
+++ js/view/counters/livemanager.js
@@ -1,5 +1,8 @@
 /**
  * Created by Jean-Baptiste on 11/04/2017.
+ * @module
+ * @description This component keeps track and displays the reamining lives of the player.
+ *
  */
 var
     LIVES_NUM = 5,
@@ -19,26 +22,50 @@ var
     };
 display(LIVES_NUM);
 module.exports = {
+    /**
+     * Removes 1 life and updates the display. Calls onLivesLost callback when 0 is reached.
+     *
+     */
     decrement: function () {
         display(--lives_num);
         if (lives_num === 0) {
             onLivesLost_fun();
         }
     },
+    /**
+     * Adds 1 life and updates the display.
+     */
     increment: function () {
         if (lives_num < LIVES_NUM) {
         display(++lives_num);
         }
     },
+    /**
+     * @type function
+     * @description Sets the callback called when the counter reaches 0. (write-only).
+     */
     set onLivesLost(fun) {
         onLivesLost_fun = fun;
     },
+    /**
+     * @type Number
+     * @readonly
+     * @description The maximum available lives.
+     */
     get maxLives () {
       return LIVES_NUM;
     },
+    /**
+     * @type Number
+     * @readonly
+     * @description The number of remaining lives.
+     */
     get lives (){
       return lives_num;
     },
+    /**
+     * Resets lives to the maximum value. Updates the display.
+     */
     reset: function () {
         lives_num = LIVES_NUM;
         display(lives_num);
--- js/view/ui/pausebutton.js
+++ js/view/ui/pausebutton.js
@@ -1,5 +1,7 @@
 /**
  * Created by Jean-Baptiste on 06/05/2017.
+ * @module
+ * @description This componenent manages the state and display of the pause button. It is managed by PauseManager.
  */
 
 var
