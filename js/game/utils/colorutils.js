c79bb41a92e1edc3b31a537092ed5f087e9a1e7c
Jean-Baptiste de Borman <jb@noloading.com>
2017-08-22 19:21:55 +0200

    Jsdoc

--- js/game/utils/colorutils.js
+++ js/game/utils/colorutils.js
@@ -1,9 +1,16 @@
 /**
  * Created by Jean-Baptiste on 29/04/2017.
+ * @module
+ * @description Utility function to handle colors
  */
 
 
 module.exports = {
+    /**
+     * Applies a "multiply" - filter
+     * @param {number} hex - The input color in hexadecimal format
+     * @param {number} percent_dec - The multiply modifier in decimal form (<1 = darker, >1 = brighter)
+     */
     multiply: function (hex, percent_dec) {
         var
             hexToRgb = function (hex) {
@@ -19,7 +26,7 @@ module.exports = {
             },
             rgb_array = hexToRgb(hex),
             result_array = [];
-        rgb_array.forEach(function (element, index) {
+        rgb_array.forEach(function (element) {
             result_array.push(Math.round(Number(element) * percent_dec));
         });
         return rgbToRGBA(result_array);
